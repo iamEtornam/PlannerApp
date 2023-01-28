23e53a997ffe73b4330a98096503bc2f8ee5857f
Etornam Sunu <sunumacbright@gmail.com>
2023-01-29 02:47:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:planner_app/views/auth_view.dart';
 import 'package:planner_app/views/todo_view.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Planner',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const TodoView(),
-    );
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting && snapshot.data == null) {
+            return MaterialApp(
+              title: 'Planner',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              ),
+            );
+          }
+          if (snapshot.data == null) {
+            return MaterialApp(
+              title: 'Planner',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: const AuthView(),
+            );
+          }
+          return MaterialApp(
+            title: 'Planner',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const TodoView(),
+          );
+        });
   }
 }
