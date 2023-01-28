b16684c41cf6622cf4cac278dc47ab318a123e77
Etornam Sunu <sunumacbright@gmail.com>
2023-01-29 02:47:40 +0300

    delete files

--- lib/bloc/todo_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'todo_bloc.dart';
-
-@immutable
-abstract class TodoEvent {}
--- lib/bloc/todo_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'todo_bloc.dart';
-
-@immutable
-abstract class TodoState {}
-
-class TodoInitial extends TodoState {}
